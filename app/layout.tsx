a6b464349febf3ae3dffabe064e1c8a906840688
naoya7076 <naoya7076@gmail.com>
2023-12-25 20:40:31 +0900

    add @clerk/localizations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
+import { jaJP } from '@clerk/localizations'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider afterSignInUrl='/' afterSignUpUrl='/onboarding'>
+    <ClerkProvider localization={jaJP} afterSignInUrl='/' afterSignUpUrl='/onboarding'>
     <html lang="ja">
       <body className={inter.className}>{children}</body>
       </html>
